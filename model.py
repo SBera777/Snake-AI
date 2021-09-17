012b520fd8c751eea15296bed9c660a07fa175ef
Corvus <47604183+Corvus-AI@users.noreply.github.com>
2021-09-18 02:19:34 +0530

    Update model.py

--- model.py
+++ model.py
@@ -36,46 +36,13 @@ class Linear_QNet(nn.Module):
                           zip(current_model_dict.keys(), loaded_state_dict.values())}
         self.load_state_dict(new_state_dict, strict=False)
 
-
-class CNN_QNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super().__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x = F.relu(self.linear1(x))
-        x = self.linear2(x)
-        return x
-
-    def save(self, file_name='model.pth'):
-        model_folder_path = './model'
-        if not os.path.exists(model_folder_path):
-            os.makedirs(model_folder_path)
-
-        file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict(), file_name)
-
-    def load(self, file_name='model-old.pth'):
-        model_folder_path = './model'
-        if not os.path.exists(model_folder_path):
-            os.makedirs(model_folder_path)
-
-        file_name = os.path.join(model_folder_path, file_name)
-        current_model_dict = self.state_dict()
-        loaded_state_dict = torch.load(file_name)
-        new_state_dict = {k: v if v.size() == current_model_dict[k].size() else current_model_dict[k] for k, v in
-                          zip(current_model_dict.keys(), loaded_state_dict.values())}
-        self.load_state_dict(new_state_dict, strict=False)
-
-
 class QTrainer:
     def __init__(self, model, lr, gamma):
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        #torch.set_default_tensor_type('torch.cuda.FloatTensor')
         self.lr = lr
         self.gamma = gamma
-        device = torch.device("cuda:0")
-        model.to(device)
+        #device = torch.device("cuda:0")
+        #model.to(device)
         self.model = model
         self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.MSELoss()
